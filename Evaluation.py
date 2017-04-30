b838779df58a9f0bba7214b51be7ea2a1c2d03ae
ZhanruoQu <quzo@ims.uni-stuttgart.de>
2017-04-30 10:58:39 +0200

    finish

--- Evaluation.py
+++ Evaluation.py
@@ -5,18 +5,45 @@ Author: Zhanruo Qu
 def IO_function(file):
     labels = list()
     for line in file:
-        labels.append(line)
+        labels.append(line.strip())
     return labels
 
-def Evaluation(gold_labels,pre_labels):
-    TP = 0
-    TN = 0
-    FP = 0
-    FN = 0
-    for num in len(gold_labels):
-        if gold_labels(num) == pre_labels(num):
-            TP += 1
-        else:
-            FN += 1
+def get_emotions(list_gold):
+    emotions = list(set(list_gold))
+    return  emotions
 
+def emo_evaluation(emotion,list_gold,list_prediction):
+    tp = 0
+    fp = 0
+    fn = 0
+    tn = 0
+    for i in range(len(list_gold)):
+        if list_gold[i] == emotion:
+            if emotion == list_prediction[i]:
+                tn += 1
+            else:
+                fn += 1
+        else:
+            if emotion == list_prediction[i]:
+                tp += 1
+            else:
+                fp += 1
+    print (tp,fp,fn,tn)
+    p = tp / (tp + fp)
+    print (p)
+    r = tp / (tp + fn)
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    f1_score = 2 * (p * r) / (p + r)
+    result = [p , r , accuracy ,f1_score]
+    return result
+
+File_gold = open("dev.txt")
+File_prediction = open("dev-predicted.csv")
+Labels_gold = IO_function(File_gold)
+Labels_prediction = IO_function(File_prediction)
+emotions = get_emotions(Labels_gold)
+for emotion in emotions:
+    print (emotion)
+    p , r , accuarcy, f1_score =  emo_evaluation(emotion, Labels_gold , Labels_prediction)
+    print ("p",p,"r",r,"accuracy",accuarcy,"f1-score",f1_score)
 
